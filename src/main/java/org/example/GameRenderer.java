1787ba3b2aaa9fb6bddcbc1b8f053325d12bd6f8
Patricija Legataitė <p.legataite@gmail.com>
2025-11-02 21:51:36 +0200

    Refactured GameRenderer Class - method was too big

--- src/main/java/org/example/GameRenderer.java
+++ src/main/java/org/example/GameRenderer.java
@@ -22,33 +22,62 @@ public class GameRenderer {
 
     private String renderToString() {
         StringBuilder screenBuffer = new StringBuilder();
+        renderHeader(screenBuffer);
+        renderGameGrid(screenBuffer);
+        renderControls(screenBuffer);
+        return screenBuffer.toString();
+    }
+
+    private void renderHeader(StringBuilder screenBuffer) {
         screenBuffer.append("Lives: ").append(robot.getLives()).append(" ");
         screenBuffer.append("Keys: ").append(robot.getKeys()).append("\n\n");
+    }
+
+    private void renderGameGrid(StringBuilder screenBuffer) {
         for (int i = 0; i < map.getHeight(); i++) {
-            for (int j = 0; j < map.getWidth(); j++) {
-                int dx = Math.abs(i - robot.getX());
-                int dy = Math.abs(j - robot.getY());
-                if (dx <= visionRadius && dy <= visionRadius) {
-                    if (i == robot.getX() && j == robot.getY()) {
-                        screenBuffer.append("R");
-                    } else if (isObstacleAt(i, j)) {
-                        MovingObstacle obstacle = getObstacleAt(i, j);
-                        if (obstacle instanceof DisappearingMovingObstacle) {
-                            screenBuffer.append(((DisappearingMovingObstacle) obstacle).getDisplaySymbol());
-                        } else {
-                            screenBuffer.append("@");
-                        }
-                    } else {
-                        screenBuffer.append(map.getModel()[i][j]);
-                    }
-                } else {
-                    screenBuffer.append("?");
-                }
-            }
+            renderGridRow(screenBuffer, i);
             screenBuffer.append("\n");
         }
+    }
+
+    private void renderGridRow(StringBuilder screenBuffer, int row) {
+        for (int j = 0; j < map.getWidth(); j++) {
+            renderGridCell(screenBuffer, row, j);
+        }
+    }
+
+    private void renderGridCell(StringBuilder screenBuffer, int x, int y) {
+        int dx = Math.abs(x - robot.getX());
+        int dy = Math.abs(y - robot.getY());
+
+        if (dx <= visionRadius && dy <= visionRadius) {
+            renderVisibleCell(screenBuffer, x, y);
+        } else {
+            screenBuffer.append("?"); // Fog of war
+        }
+    }
+
+    private void renderVisibleCell(StringBuilder screenBuffer, int x, int y) {
+        if (x == robot.getX() && y == robot.getY()) {
+            screenBuffer.append("R");
+        } else if (isObstacleAt(x, y)) {
+            renderObstacleCell(screenBuffer, x, y);
+        } else {
+            screenBuffer.append(map.getModel()[x][y]);
+        }
+    }
+
+    private void renderObstacleCell(StringBuilder screenBuffer, int x, int y) {
+        MovingObstacle obstacle = getObstacleAt(x, y);
+        if (obstacle instanceof DisappearingMovingObstacle) {
+            screenBuffer.append(((DisappearingMovingObstacle) obstacle).getDisplaySymbol());
+        } else {
+            screenBuffer.append("@");
+        }
+    }
+
+    private void renderControls(StringBuilder screenBuffer) {
         screenBuffer.append("\n[WASD] + [Enter] to move, [Q] to quit.\n");
-        return screenBuffer.toString();
     }
 
     private boolean isObstacleAt(int x, int y) {
--- src/main/java/org/example/Robot.java
+++ src/main/java/org/example/Robot.java
@@ -7,6 +7,7 @@ public class Robot {
     private int keys = 0;
     private int lives = 3;
 
+
     public Robot(int x, int y) {
         this.x = x;
         this.y = y;
